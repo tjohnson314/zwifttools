bb6d154d8bc96a349a0bee2cf8ef369c843b115c
Tim Johnson <timjohnson314@gmail.com>
2026-02-13 23:28:18 -0800

    Set PYTHONPATH in startup.sh for bundled dependencies

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Azure App Service startup script
+
+# Add bundled dependencies to Python path
+export PYTHONPATH="/home/site/wwwroot/.python_packages/lib/site-packages:$PYTHONPATH"
+
 gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers 2 app:app
