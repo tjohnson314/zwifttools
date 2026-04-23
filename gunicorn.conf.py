a123c8979d8fe862e1bc40b7d237657afdbafdf3
Tim Johnson <timjohnson314@gmail.com>
2026-04-23 13:42:52 -0700

    Fix issues with loading individual rider data

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 """Gunicorn configuration for Azure App Service."""
 
 bind = "0.0.0.0:8000"
-workers = 2
+workers = 1
 threads = 4
 timeout = 600  # Long timeout for Zwift API calls
 accesslog = "-"
